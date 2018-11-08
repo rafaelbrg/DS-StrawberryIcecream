7c1d63abf4842092e40f8cef94c4950e0d2fdcb2
Filipe Machado <filipe__machado@hotmail.com>
2018-11-07 22:08:42 -0200

    Prototipo de funções de comunicação entre scripts

---
+++ background_script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+chrome.runtime.onMessage.addListener(receiver);
+
+	function receiver(request, sender, sendResponse){
+		
+		console.log(request);
+		//request é a mensagem recebida
+              //Aqui deve ser oq o programa deve fazer depois que receber a mensagem
+		
+	}	
+		
+}
+
